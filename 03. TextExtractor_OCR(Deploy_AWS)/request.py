79e8aed66f8d7bb69846cb6a49ca70a18050b8ea
GirrajJangid <girraj.jangid.14581@gmail.com>
2019-11-25 23:30:57 +0530

    remaining update

--- 03. TextExtractor_OCR(Deploy_AWS)/request.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = 'http://localhost:5000/extract_date'
-with open('base24_2.txt','r') as fl:
-    data = fl.read()
-r = requests.post(url,json=[{'base_64_image_content':data}])
-print(r.json())
